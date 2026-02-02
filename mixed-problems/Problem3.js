aedd62d0f3917ab7d671f85a93895b50587d39c2
Ashraf Ali <ashrafmohd83517@gmail.com>
2026-02-02 14:18:28 +0530

    3rd problem solved

---
+++ mixed-problems/Problem3.js
@@ -0,0 +1,22 @@
+/*
+Problem #3: Sum of All Numbers
+
+Write a function that takes an array of numbers
+and returns the sum of all numbers.
+
+Rules:
+- Use a loop
+- Do NOT use reduce() or other built-in sum functions
+
+Example:
+sumArray([1, 2, 3, 4])  // Output: 10
+*/
+
+function sumOfArray(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+console.log(sumOfArray([1, 2, 3, 4, 5]));
